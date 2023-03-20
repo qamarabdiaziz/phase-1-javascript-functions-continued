408ee4359cba1a53753cea19044127adbd81edf2
qamarabdiaziz <tushez98@gmail.com>
2023-03-20 19:21:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+  function saturdayFun (activity ='roller-skate'){
+    return `This Saturday, I want to roller-skate!`
+  }
+saturdayFun(bathe );//"This Saturday, I want to bathe my dog"
+
+const MondayWork = function(activity ="go to the office"){
+  return `This Monday,I will $(activity)`
+}
+
+MondayWork("work from home");//  "This Monday,I will work from home."
+MondayWork("have a meeting");// "This Monday,I will have a meeting."
+MondayWork();//"This Monday,I will go to the office."
+
+
+
+
